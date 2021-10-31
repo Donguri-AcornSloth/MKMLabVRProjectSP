1c3e52b5db58b87a68d6a4702357ebdf543c7e77
Araki <m0a20014@edu.teu.ac.jp>
2021-10-31 14:38:25 +0900

    スクリプトとプレハブのアップロード

---
+++ Scripts/TargetManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public ScoreManager scoreManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            // TargetとBulletを両方消去
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+
+            // 得点を追加
+            scoreManager.AddScore();
+        }
+    }
+}
