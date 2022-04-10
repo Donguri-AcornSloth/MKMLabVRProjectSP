5f5d7c546d497ea1aedf5fb5623cde931d4c88eb
KaitoAraki612 <m0a20014bc@edu.teu.ac.jp>
2022-04-10 16:29:32 +0900

    ボタンSEの実装

--- Scripts/ToStage1.cs
+++ Scripts/ToStage1.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ToStage1 : MonoBehaviour
 {
+    public AudioClip ToStage1SE;
+    private AudioSource AudioSource;
+
+    void Start()
+    {
+        AudioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
     public void OnClickButton()
     {
+        //音・パーティクルを再生
+        AudioSource.PlayOneShot(ToStage1SE);
         SceneManager.LoadScene("Stage1_Test");
     }
 }
--- Scripts/ToStageSelect.cs
+++ Scripts/ToStageSelect.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ToStageSelect : MonoBehaviour
 {
+    public AudioClip ToStageSelectSE;
+    private AudioSource AudioSource;
+
+    void Start()
+    {
+        AudioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
     public void OnClickButton()
     {
+        //音・パーティクルを再生
+        AudioSource.PlayOneShot(ToStageSelectSE);
         SceneManager.LoadScene("StageSelection");
     }
 }
