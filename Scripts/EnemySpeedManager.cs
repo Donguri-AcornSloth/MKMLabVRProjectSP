8f3153c9af4aa8d6bf9077748ed2476e0d1d9e42
akamatsu honoka <honoaka1001@gmail.com>
2022-06-22 15:14:27 +0900

    Enemyの動きに関するPathとTrigger

---
+++ Scripts/EnemySpeedManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpeedManager : MonoBehaviour
+{
+    public float triggerSpeed;
+    public float setSpeed;
+    public float waitTime;
+
+    GameObject enemy;
+    Cinemachine.CinemachineDollyCart script;
+
+    void Start()
+    {
+        enemy = GameObject.Find("Enemy");
+        script = enemy.GetComponent<Cinemachine.CinemachineDollyCart>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            script.m_Speed = triggerSpeed;
+            StartCoroutine("NextArea");
+        }
+    }
+
+    IEnumerator NextArea()
+    {
+        yield return new WaitForSeconds(waitTime);
+        script.m_Speed = setSpeed;
+    }
+}
